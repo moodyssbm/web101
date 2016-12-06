eadf1a1464ed1c5e4793a4572a25c30ee76417e2
moodyssbm <moodyssbm@gmail.com>
2016-12-06 12:31:03 -0500

    going to bed for today

--- final/js/lorem.js
+++ final/js/lorem.js
@@ -1,11 +1,15 @@
-let container = document.getElementById('container');
+let mainCont = document.getElementById('mainCont');
+let head = document.getElementById('head');
 
 let long = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ';
 
 let short = 'Lorem ipsum dolor sit amet. ';
 
+let single = ['Lorem', 'Ipsum', 'Dolor', 'Dit', 'Amet'];
+
 function lorem(x, len) {
   let result = '';
+
 
   if(len == 'short') {
     for(i=0; i!=x; i++) {
@@ -16,9 +20,19 @@ function lorem(x, len) {
       result += long;
     }
   }
-  return result
+  return result;
 }
 
 window.onload = function() {
-  container.innerHTML += lorem(5);
+  mainCont.innerHTML = '<p>';
+  head.innerHTML = '';
+  mainCont.innerHTML += lorem(5);
+  mainCont.innerHTML += '</p>';
+  mainCont.innerHTML += lorem(2);
+  mainCont.innerHTML += '</p>';
+  mainCont.innerHTML += lorem(3);
+  mainCont.innerHTML += '</p>';
+  head.innerHTML += lorem(1, 'short');
 }
+
+// god bless js for dummy text generation
